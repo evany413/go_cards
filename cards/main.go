a3ae88bab000357f1729ca2884ad32f672892e60
Evan Yang <evan.yang@sisu.edu>
2024-07-17 20:38:00 -0700

    add feature: output to file and read from a file

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,8 @@
 package main
 
 func main() {
-	// var card string = "Ace of Spades"
-	// card := "Ace of Spades"
-	// card := newCard()
-
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeck()
+	// cards.saveToFile("my_cards")
+	cards = newDeckFromFile("my_cards")
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
