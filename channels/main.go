44fab9d1848ea016d16e7e4c6bd409944a01e064
Evan Yang <evan.yang@sisu.edu>
2024-07-19 15:39:02 -0700

    add feature: concurrently check website status

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,18 +15,45 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		// checkLink(link)
+		go checkLink(link, c) // go keyword creates a new go routine
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	// main routine sleeps for 5 seconds
+	// for l := range c {
+	// 	go checkLink(l, c)
+	// 	time.Sleep(5 * time.Second)
+	// }
+
+	// go routine sleeps for 5 seconds
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
