82f410acea0f4d76e3968dbdaf25af3bf00f69ca
Evan Yang <evan.yang@sisu.edu>
2024-07-19 01:13:55 -0700

    add custom type logWriter

--- http/main.go
+++ http/main.go
@@ -7,13 +7,21 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://www.google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
 
-	io.Copy(os.Stdout, resp.Body)
+}
 
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
